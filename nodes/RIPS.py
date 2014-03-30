a279cf5cc3ab65d7c8d3f9e3e2ea2d61acd90b4e
Krit Chaiso <krit.c@ku.th>
2014-03-30 15:37:13 +0700

    Modify read_location function

--- nodes/RIPS.py
+++ nodes/RIPS.py
@@ -1,82 +1,76 @@
 #!/usr/bin/env python
 import rospy
 import roslib
-import time
-from subprocess import call
-roslib.load_manifest('main_state')
-
 from include.function import *
 from include.publish import *
 from include.base_state import *
-from std_msgs.msg import String
-from lumyai_navigation_msgs.msg import NavGoalMsg
-from geometry_msgs.msg import Quaternion, Pose2D
 from math import pi
 
-location_list = {}
+roslib.load_manifest('main_state')
 
 class RIPS(BaseState):
-	def __init__(self):
-		BaseState.__init__(self)
+    def __init__(self):
+        BaseState.__init__(self)
 
-		Publish.set_height(1.27)
-		rospy.loginfo('Start RIPS State')
-		rospy.spin()
+        Publish.set_height(1.27)
+        rospy.loginfo('Start RIPS State')
+        rospy.spin()
 
-	def main(self, device, data):
-		rospy.loginfo("state:"+self.state+" from:"+device+" data:"+str(data))
-		if(self.state == 'init'):
-			if(device == 'door' and data == 'open'):
-				# move pass door
-				Publish.move_relative(1.5,0)
-				self.state = 'passDoor'
-			publish.pan_tilt_command(getQuaternion(0,50*pi/180,0))
-			publish.manipulator_action.publish(String('walking'))
-		elif(state == 'passDoor'):
-			if(device == 'base' and data == 'SUCCEEDED'):
-				state = 'gotoTable'
-				# send to base
-				publish.base.publish(location_list['register_pos'])
-		elif(state == 'gotoTable'):
-			if(device == 'base' and data == 'SUCCEEDED'):
-				state = 'moveArm'
-				# send to arm
-				publish.manipulator_action.publish(String('rips_out'))
-				publish.pan_tilt_command(getQuaternion(0,0,0))
-				call(["espeak","-ven+f4","Hello Sir,My name is lumyai,i came from kasetsart university thailand.i am a robot from planet earth came here to service you please accept this registration","-s 120"])
-		elif(state == 'moveArm'):
-			if(device == 'manipulator' and data == 'finish'):
-				state = 'waitForCommand'
-				
-		elif(state == 'waitForCommand'):
-			if(device == 'voice'):
-				if(data == 'get out'):
-					state = 'arm out'
-					# send to arm
-					publish.manipulator_action.publish(String('walking'))
-		elif(state == 'arm out'):
-			if(device == 'manipulator' and data == 'finish'):
-				# send to pan_tilt
-				publish.pan_tilt_command(getQuaternion(0,50*pi/180,0))
-				delay.delay(2)
-				# send to base
-				publish.base.publish(location_list['outside_pos'])
-				state = 'get out'
+    def main(self, device, data):
+        rospy.loginfo("state:" + self.state + " from:" + device + " data:" + str(data))
+        if self.state == 'init':
+            if device == Devices.door and data == 'open':
+                # move pass door
+                Publish.move_relative(1.5, 0)
+                self.state = 'passDoor'
+            Publish.set_neck(0, 50*pi/180,0)
+            Publish.set_manipulator_action('walking')
+        elif self.state == 'passDoor':
+            if device == Devices.base and data == 'SUCCEEDED':
+                self.state = 'gotoTable'
+                # send to base
+                self.move_robot('register_pos')
+        elif self.state == 'gotoTable':
+            if device == Devices.base and data == 'SUCCEEDED':
+                state = 'moveArm'
+                # send to arm
+                Publish.set_manipulator_action('rips_out')
+                Publish.set_neck(0, 0, 0)
+                Publish.speak("Hello Sir, My name is Lumyai. I came from Kasetsart University Thailand. I am a robot from planet earth, came here to service you. Please accept this registration.")
+        elif (state == 'moveArm'):
+            if (device == 'manipulator' and data == 'finish'):
+                state = 'waitForCommand'
 
-		elif(state == 'set neck'):
-			if(device == 'servo' and data == 'finish'):
-				# send to base
-				publish.base.publish(location_list['outside_pos'])
-				state = 'get out'
-		elif(state == 'get out'):
-			publish.pan_tilt_command(getQuaternion(0,50*pi/180,0))
-			publish.manipulator_action.publish(String('walking'))
-			if(device == 'base' and data == 'SUCCEEDED'):
-				state = 'finish'
+        elif (state == 'waitForCommand'):
+            if (device == 'voice'):
+                if (data == 'get out'):
+                    state = 'arm out'
+                    # send to arm
+                    publish.manipulator_action.publish(String('walking'))
+        elif (state == 'arm out'):
+            if (device == 'manipulator' and data == 'finish'):
+                # send to pan_tilt
+                publish.pan_tilt_command(getQuaternion(0, 50 * pi / 180, 0))
+                delay.delay(2)
+                # send to base
+                publish.base.publish(location_list['outside_pos'])
+                state = 'get out'
+
+        elif (state == 'set neck'):
+            if (device == 'servo' and data == 'finish'):
+                # send to base
+                publish.base.publish(location_list['outside_pos'])
+                state = 'get out'
+        elif (state == 'get out'):
+            publish.pan_tilt_command(getQuaternion(0, 50 * pi / 180, 0))
+            publish.manipulator_action.publish(String('walking'))
+            if (device == 'base' and data == 'SUCCEEDED'):
+                state = 'finish'
+
 
 if __name__ == '__main__':
     try:
-    	rospy.init_node('main_state')
-	RIPS()
+        rospy.init_node('main_state')
+        RIPS()
     except Exception, error:
         print str(error)
--- nodes/include/base_state.py
+++ nodes/include/base_state.py
@@ -1,46 +1,55 @@
 #!usr/bin/env python
 import rospy
 import roslib
-roslib.load_manifest('main_state')
-
 from std_msgs.msg import String
 from delay import *
+from publish import *
+roslib.load_manifest('main_state')
+
+
+
 
 class Devices:
-	door='door'
-	base='base'
-	manipulator='manipulator'
-	voice='voice'
+    door = 'door'
+    base = 'base'
+    manipulator = 'manipulator'
+    voice = 'voice'
+
 
 class BaseState:
-	def __init__(self):
-		rospy.Subscriber('/door/is_open', String, self.CallbackDoor)
-		rospy.Subscriber('/base/is_fin', String, self.CallbackBase)
-		rospy.Subscriber('/manipulator/is_fin', String, self.CallbackManipulator)
-		rospy.Subscriber('/voice/output', String, self.CallbackVoice)
+    def __init__(self):
+        rospy.Subscriber('/door/is_open', String, self.CallbackDoor)
+        rospy.Subscriber('/base/is_fin', String, self.CallbackBase)
+        rospy.Subscriber('/manipulator/is_fin', String, self.CallbackManipulator)
+        rospy.Subscriber('/voice/output', String, self.CallbackVoice)
 
-		self.delay = Delay()
-		self.state = 'init'
+        self.delay = Delay()
+        self.location_list = {}
+        read_location(self.location_list)
+        self.state = 'init'
 
-	def CallbackDoor(self,data):
-		self.main(Devices.door,data.data)
+    def callback_door(self, data):
+        self.main(Devices.door, data.data)
 
-	def CallbackBase(self,data):
-		self.main(Devices.base,data.data)
-		
-	def CallbackManipulator(self,data):
-		self.main(Devices.manipulator,data.data)
-		
-	def CallbackVoice(self,data):
-		self.main(Devices.voice,data.data)
+    def callback_base(self, data):
+        self.main(Devices.base, data.data)
 
-	def main(self,device,data):
-		pass
-		
+    def callback_manipulator(self, data):
+        self.main(Devices.manipulator, data.data)
+
+    def callback_voice(self, data):
+        self.main(Devices.voice, data.data)
+
+    def main(self, device, data):
+        pass
+
+    def move_robot(self, location):
+        Publish.move_absolute(self.location_list[location].pose)
+
 
 if __name__ == '__main__':
-	try:
-		BaseState()
-	except Exception, error:
-		print str(error)
+    try:
+        BaseState()
+    except Exception, error:
+        print str(error)
 	
