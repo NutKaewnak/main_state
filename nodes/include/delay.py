1c0a3cbdbd528c0b3d6e539799f24889c41ad6a8
oske <oske@localhost>
2014-04-20 20:55:29 +0700

    Remove delay.

--- nodes/include/delay.py
+++ nodes/include/delay.py
@@ -6,37 +6,14 @@ class Delay:
     def __init__(self):
         self.start_time = time.localtime()
         self.period = 0
-        self.wait_period = 0
 
-    # in second
-    def delay(self, period):
-        self.start_time = time.localtime()
+    def wait(self, period):
         self.period = period
-        time.sleep(period)
-
-    def waiting(self, period):
-        self.wait_period = period
         self.start_time = time.localtime()
-        time.sleep(1)
 
-    def isWaitFinish(self):
+    def is_waiting(self):
         current_time = time.localtime()
-        if (time.mktime(current_time) - time.mktime(self.start_time) >= self.wait_period):
-            return True
-        return False
-
-    def isWait(self):
-        current_time = time.localtime()
-        if (time.mktime(current_time) - time.mktime(self.start_time) >= self.period):
+        if time.mktime(current_time) - time.mktime(self.start_time) >= self.period:
             return False
         return True
 
-
-if __name__ == "__main__":
-    object_list = {}
-    readObject(object_list)
-    print object_list
-    location_list = {}
-    readLocation(location_list)
-    print location_list
-	
