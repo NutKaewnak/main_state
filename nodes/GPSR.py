054be6ecece12f1f9584d7e6d89aa8da52302b42
oske <oske@localhost>
2014-04-21 08:47:56 +0700

    Add state ask object location.

--- nodes/GPSR.py
+++ nodes/GPSR.py
@@ -32,6 +32,7 @@ class GPSR(BaseState):
         lc_filename = roslib.packages.get_pkg_dir('main_state') + '/config/command_config/location_categories.txt'
         oc_filename = roslib.packages.get_pkg_dir('main_state') + '/config/command_config/object_categories.txt'
         ol_filename = roslib.packages.get_pkg_dir('main_state') + '/config/command_config/object_locations.txt'
+        self.state = 'wait'
         self.command = ''
         self.command_extractor = CommandExtractor()
         self.current_action_index = 0
@@ -41,7 +42,8 @@ class GPSR(BaseState):
         self.location_categories = readFileToDic(lc_filename)
         self.object_categories = readFileToDic(oc_filename)
         self.object_locations = readFileToDic(ol_filename)
-        self.ask_data = ''
+        self.ask_data = None
+        self.object_location = None
         rospy.spin()
 
     def findObjectCategory(self, object):
@@ -69,15 +71,17 @@ class GPSR(BaseState):
                 self.startMoving(self.current_action)
                 #rospy.loginfo('%s,%s,%s'%(action.action,action.object if action.object else 'None',action.data if action.data else 'None'))
             elif action.data in self.location_categories.keys():
-                Publish.speak("Please tell me where %s is." % action.data)
+                self.speak("Please tell me where %s is." % action.data)
                 self.state = 'ask_data'
-        if action.action in self.verb_categories['bring']:
+        elif action.action in self.verb_categories['bring']:
             if action.object in [obj for k in self.object_categories.keys() for obj in self.object_categories[k]]:
                 self.startMoving(self.current_action)
                 #rospy.loginfo('%s,%s,%s'%(action.action,action.object if action.object else 'None',action.data if action.data else 'None'))
             elif action.object in self.object_categories.keys():
-                Publish.speak("Please tell me what %s to bring." % action.object)
+                self.speak("Please tell me what %s to bring." % action.object)
                 self.state = 'ask_object'
+        elif action.action in self.verb_categories['follow']:
+            self.state = 'follow'
 
     def finishAction(self):
         if self.current_action_index + 1 < len(self.actions):
@@ -91,18 +95,26 @@ class GPSR(BaseState):
             self.state = 'return'
 
     def main(self, device, data):
-        rospy.loginfo('state in:' + self.state + ' from:' + device + ' data:' + str(data) + ' wait: ' + str(self.delay.isWait()))
-        if self.delay.isWait():
-            return None
+        rospy.loginfo('state in:' + self.state + ' from:' + device + ' data:' + str(data))
         if self.state == 'init':
+            if device == Devices.door and data == 'open':
+                Publish.move_relative(1.5,0)
+                self.state = 'pass_door'
+        elif self.state == 'pass_door':
+            if device == Devices.base and data == 'SUCCEEDED':
+                self.move_robot('start_pos')
+                self.state = 'go_to_start'
+        elif self.state == 'go_to_start':
+            if device == Devices and data == 'SUCCEEDED':
+                self.state = 'wait'
+        elif self.state == 'wait':
             if device == Devices.voice and self.command_extractor.isValidCommand(data):
                 self.command = data
-                Publish.speak('Do you want me to %s' % data)
+                self.speak('Do you want me to %s' % data)
                 self.state = 'confirm'
         elif self.state == 'confirm':
             if device == Devices.voice:
                 if 'yes' in data:
-                    #rospy.loginfo(self.command)
                     actions = self.command_extractor.getActions(self.command)
                     #rospy.loginfo('Num action : ' + str(len(actions)))
                     for i in xrange(0,len(actions)):
@@ -110,49 +122,58 @@ class GPSR(BaseState):
                         #rospy.loginfo(' - %s,%s,%s'%(action.action,action.object if action.object else 'None',action.data if action.data else 'None'))
                         self.actions.append(action)
                     self.current_action = self.actions[self.current_action_index]
-                    Publish.speak('I will %s.' % self.command)
+                    self.speak('I will %s.' % self.command)
                     self.startAction(self.current_action)
                 elif 'no' in data:
-                    Publish.speak('Please repeat your command.')
+                    self.speak('Please repeat your command.')
                     self.state = 'init'
         elif self.state == 'ask_object':
             if device == Devices.voice and data in self.object_categories[self.current_action.object]:
-                Publish.speak('%s you want is %s yes or no.' % (self.current_action.object,data))
+                self.speak('%s you want is %s yes or no.' % (self.current_action.object,data))
                 self.ask_data = data
                 self.state = 'ask_object_confirm'
         elif self.state == 'ask_object_confirm':
             if device == Devices.voice:
                 if 'yes' in data:
                     self.current_action.object = self.ask_data
-                    if self.current_action.data in self.location_categories.keys():
-                        Publish.speak('Please tell me where %s is.' % self.current_action.data)
-                        self.state = 'ask_data'
-                    else:
-                        #rospy.loginfo('I will %s %s' % (self.current_action.action,self.current_action.object))
-                        Publish.speak('I will %s %s' % (self.current_action.action,self.current_action.object))
-                        self.startMoving(self.current_action)
-                        self.state = 'move'
-                        rospy.loginfo('%s,%s,%s'%(self.current_action.action,self.current_action.object if self.current_action.object else 'None', self.current_action.data if self.current_action.data else 'None'))
+                    self.speak('Please tell me where %s is.'%self.current_action.object)
+                    self.state = 'ask_object_location'
                 elif 'no' in data:
-                    if self.current_action.object in self.object_categories.keys():
-                        Publish.speak('Please tell me what %s to bring.' % self.current_action.object)
+                    self.speak('Please tell me what %s to bring.' % self.current_action.object)
                     self.state = 'ask_object'
+        elif self.state == 'ask_object_location':
+            if device == Devices.voice:
+                self.speak('%s is at %s yes or no.'%(self.current_action.object,data))
+                self.ask_data = data
+                self.state = 'ask_object_location_confirm'
+        elif self.state == 'ask_object_location_confirm':
+            if device == Devices.voice and 'yes' in data:
+                self.object_location = self.ask_data
+                if self.current_action.data:
+                    self.speak('Please tell me where %s is.'%self.current_action.data)
+                    self.state = 'ask_data'
+                else:
+                    rospy.loginfo('%s %s %s %s'%(self.current_action.action,self.current_action.object,self.current_action.data,self.object_location))
+                    self.speak('I will %s %s'%(self.current_action.action,self.current_action.object))
+                    self.move_robot(self.object_location)
+                    self.state = 'move'
         elif self.state == 'ask_data':
             if device == Devices.voice and data in self.location_categories[self.current_action.data]:
-                Publish.speak('%s is at %s yes or no.' % (self.current_action.data,data))
+                self.speak('%s is at %s yes or no.' % (self.current_action.data,data))
                 self.ask_data = data
                 self.state = 'ask_data_confirm'
         elif self.state == 'ask_data_confirm':
             if device == Devices.voice:
                 if 'yes' in data:
+                    rospy.loginfo('%s %s %s %s'%(self.current_action.action,self.current_action.object,self.current_action.data,self.object_location))
                     self.current_action.data = self.ask_data
                     #rospy.loginfo('I will %s %s to %s' % (self.current_action.action,self.current_action.object if self.current_action.object else '',self.current_action.data if self.current_action.data else ''))
-                    Publish.speak('I will %s %s to %s' % (self.current_action.action,self.current_action.object if self.current_action.object else '',self.current_action.data if self.current_action.data else ''))
+                    self.speak('I will %s %s to %s' % (self.current_action.action,self.current_action.object if self.current_action.object else '',self.current_action.data if self.current_action.data else ''))
                     self.startMoving(self.current_action)
                     self.state = 'move'
                     #rospy.loginfo('%s,%s,%s'%(self.current_action.action,self.current_action.object if self.current_action.object else 'None', self.current_action.data if self.current_action.data else 'None'))
                 elif 'no' in data:
-                    Publish.speak('Please tell me where %s is' % self.current_action.data)
+                    self.speak('Please tell me where %s is' % self.current_action.data)
                     self.state = 'ask_data'
         elif self.state == 'move':
             if device == Devices.base and data == 'SUCCEEDED':
@@ -169,7 +190,7 @@ class GPSR(BaseState):
                     #self.move_robot(self.current_action.data)
                     self.state = 'deliver'
                 else:
-                    Publish.speak('I reach the %s'%self.current_action.data)
+                    self.speak('I reach the %s'%self.current_action.data)
                     self.finishAction()
         elif self.state == 'object_search':
             if device == 'object':
@@ -179,7 +200,7 @@ class GPSR(BaseState):
                     self.delay.waiting(2)
                     self.state = 'get_object'
                 elif data == 'no':
-                    Publish.speak(self.current_action.object + ' not found.')
+                    self.speak(self.current_action.object + ' not found.')
                     self.move_robot('start_pos')
                     self.delay.waiting(2)
                     self.state = 'return'
@@ -192,7 +213,7 @@ class GPSR(BaseState):
                 self.state = 'deliver'
         elif self.state == 'deliver':
             if device == Devices.base and data == 'SUCCEEDED':
-                Publish.speak('This is %s.'%self.current_action.object)
+                self.speak('This is %s.'%self.current_action.object)
                 #Publish.set_manipulator_action('rips_out')
                 #self.state = 'give'
                 self.finishAction()
@@ -201,11 +222,11 @@ class GPSR(BaseState):
                 self.finishAction()
         elif self.state == 'return':
             if device == Devices.base and data == 'SUCCEEDED':
-                Publish.speak('Return to starting point')
+                self.speak('Return to starting point')
                 self.actions = []
                 self.current_action_index = 0
                 self.current_action = None
-                self.state = 'init'
+                self.state = 'wait'
 
 if __name__ == '__main__':
     try:
