30a011a291c4744ebb18dc41ff00eb38ec08ec23
jidrids <jidrids@localhost>
2014-04-26 23:26:10 +0700

    Change old grasp and pour manipulate nodes to new nodes

--- nodes/finalDemo-v2.py
+++ nodes/finalDemo-v2.py
@@ -12,11 +12,9 @@ roslib.load_manifest('main_state')
 class FINALDEMO(BaseState):
     def __init__(self):
         BaseState.__init__(self)
-        #self.findObjectPointPublisher = rospy.Publisher('/localization', String)
         self.desiredObject = 1 #NOTE 1 = cornflakes
         self.desiredObject2 = 2#NOTE 2 = milk
         Publish.set_height(1.27)
-        #rospy.Subscriber("/detected_object", ObjectContainer, self.cb_detectedObject)
         rospy.loginfo('Start Final Demo State')
         rospy.spin()
 
@@ -24,9 +22,8 @@ class FINALDEMO(BaseState):
         rospy.loginfo("state:" + self.state + " from:" + device + " data:" + str(data))
 
         if self.state == 'init':
-            if device == Devices.voice and data == 'breakfast':
+            if (device == Devices.voice and (data == 'i need breakfast'or data =='i am hugry'):
                 Publish.speak("I will make you a breakfast.")
-                #self.findObjectPointPublisher.publish(String("start"))
                 Publish.find_object(String("start"))
                 self.state = 'searchingCornflake'
 
@@ -35,7 +32,6 @@ class FINALDEMO(BaseState):
                 Publish.speak("Searching cornflake.")
                 object = []
                 if data.isMove:
-                    #to go closer
                     pass
                 else:
                     objects = data.objects
@@ -45,10 +41,9 @@ class FINALDEMO(BaseState):
                             centroidVector.x = obj.point.x
                             centroidVector.y = obj.point.y
                             centroidVector.z = obj.point.z
-                            #pickObjectPublisher.publish(centroidVector)
-                            Publish.find_object(centroidVector)
-                            #TODO debug by push out of 'if state'
-                    self.state = 'graspingCornflake'
+                            Publish.set_manipulate_grasp(centroidVector)
+                            #NOTE debug by push out of 'if state'
+                            self.state = 'graspingCornflake'
 
         elif self.state == 'graspingCornflake':
             if device == Devices.manipulator and data == 'finish':
@@ -57,26 +52,24 @@ class FINALDEMO(BaseState):
         elif self.state == 'trackingBowl':
             if device == Devices.color_detector:
                 Publish.speak("searching a bowl.")
-                #TODO subscribe position from camera (Color Detect)  **add color_detector to launch file
-                Publish.set_manipulator_point(data)
-                #Done publish position Vector3 to manipulate
+                Publish.set_manipulator_action_pour(data)
                 Publish.speak("pouring.")
                 self.state = 'discardCornflake'
 
         elif self.state == 'discardCornflake':
             if device == Devices.manipulator and data == 'finish':
                 Publish.speak("discard cornflake.")
-                #TODO manipulate drop action isn't it?
-                Publish.set_manipulator_action('walking_for_drop')
-                self.state = 'searchingMilk'
+                #deley 3 sec
+                Publish.set_manipulator_action('normal_pullback')
+                if device == Devices.manipulator and data == 'finish':
+                        Publish.findObject(String("start"))
+                        self.state = 'searchingMilk'
 
         elif self.state == 'searchingMilk':
-            if device == Devices.manipulator and data == 'finish':
                 if device == Devices.recognition:
                     Publish.speak("Searching milk")
                     object = []
                     if data.isMove:
-                        #to go closer
                         pass
                     else:
                         objects = data.objects
@@ -86,8 +79,7 @@ class FINALDEMO(BaseState):
                                 centroidVector.x = obj.point.x
                                 centroidVector.y = obj.point.y
                                 centroidVector.z = obj.point.z
-                                Publish.set_manipulator_point(centroidVector)
-                                #pickObjectPublisher.publish(centroidVector)
+                                Publish.set_manipulator_action_grasp(centroidVector)
                                 self.state = 'graspingMilk'
 
         elif self.state == 'graspingMilk':
@@ -97,27 +89,19 @@ class FINALDEMO(BaseState):
         elif self.state == 'trackingBowl_2':
             if device == Devices.color_detector:
                 Publish.speak("searching a bowl.")
-                #TODO Could we use the first point? and shall we?
-                # send_object_point.publish(data)
-                Publish.set_manipulator_point(data)
+                Publish.set_manipulator_action_pour(data)
                 Publish.speak("pouring.")
                 self.state = 'discardMilk'
 
         elif self.state == 'discardMilk':
             if device == Devices.manipulate and data == 'finish':
-                #TODO this is a pour command isn't it?
-                #Publish.set_manipulator_action('walking_for_drop')
+                #delay 3sec
+                Publish.set_manipulator_action('normal_pullback')
                 self.state = 'serve'
 
         elif self.state == 'serve':
             if device == Devices.manipulate and data == 'finish':
                 Publish.speak("the breakfast is ready served.")
-                #TODO this is a discard command isn't it?
-                Publish.set_manipulator_action('walking_for_drop')
-            self.state = 'drop'
-
-        elif self.state == 'drop':
-            if device == Devices.manipulator and data == 'finish':
                 self.state = 'finish'
 
         elif self.state == 'finish':
