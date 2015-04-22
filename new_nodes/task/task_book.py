6eb51e07f5d67004700e1d775cbd6edd30b37aa2
Nut Kaewnak <NutKaewnak@users.noreply.github.com>
2015-04-22 22:14:59 +0700

    added test.py to book

--- new_nodes/task/task_book.py
+++ new_nodes/task/task_book.py
@@ -1,12 +1,13 @@
 __author__ = "AThousandYears"
 
 from rips import RIPS
-
+from test import Test
 
 class TaskBook:
     def __init__(self, planning_module):
         self.book = {None}
         self.book['RIPS'] = RIPS(planning_module)
+        self.book['Test'] = Test(planning_module)
 
     def set_perception(self, perception_module):
         for task in self.book:
