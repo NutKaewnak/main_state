13931897000a41dc618c001a70fa891a4d877298
Nut Kaewnak <NutKaewnak@users.noreply.github.com>
2015-04-23 00:36:18 +0700

    have rips out and RIPS is done

--- new_nodes/subtask/introduce.py
+++ new_nodes/subtask/introduce.py
@@ -11,4 +11,8 @@ class Introduce(AbstractSubtask):
         if self.state is 'init':
             self.skill = self.skillBook.get_skill(self, 'Say')
             self.skill.say('Hello, my name is Lamyai. I am from Kasetsart university.')
+            self.change_state('rips_out')
+
+        elif self.state is 'rips_out':
+            self.skill = self.skillBook.get_skill(self, 'RipsOut')
             self.change_state('succeed')
