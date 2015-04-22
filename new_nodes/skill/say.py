478bd4eac96df296083fb25c7f0b62621dd99aac
Nut Kaewnak <NutKaewnak@users.noreply.github.com>
2015-04-22 22:21:13 +0700

    edit say

--- new_nodes/skill/say.py
+++ new_nodes/skill/say.py
@@ -1,8 +1,12 @@
 __author__ = 'nicole'
 from include.abstract_skill import AbstractSkill
+
+
 class Say(AbstractSkill):
-    def __init__(self):
-        AbstractSkill.__init__(self)
+    def __init__(self, control_module):
+        AbstractSkill.__init__(self, control_module)
 
     def say(self, message):
-        self.controlModule.speaker.speak(message)
+        if self.state is 'init':
+            self.controlModule.speaker.speak(message)
+            self.change_state('succeed')
