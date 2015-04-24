01bc99fbc9ed68e7ae8d818ddf5a9186b39c28a7
nicole <nicole@Nuts-MacBook-Pro.local>
2015-04-25 06:09:50 +0700

    Fix BasicFunctionality stage and added LeaveArena subtask

---
+++ new_nodes/subtask/leave_arena.py
@@ -0,0 +1,26 @@
+__author__ = 'nicole'
+
+
+import rospy
+
+from include.abstract_subtask import AbstractSubtask
+
+
+class LeaveArena(AbstractSubtask):
+    def __init__(self, planning_module):
+        AbstractSubtask.__init__(self, planning_module)
+        self.skill = self.current_skill
+        self.subtask = self.current_subtask
+
+    def perform(self, perception_data):
+        if self.state is 'init':
+            self.subtask = self.subtaskBook.get_subtask('MoveToLocation')
+            self.subtask.to_Location('exit')
+            self.change_state('MoveToExit')
+        elif self.state is 'MoveToExit':
+            if self.subtask.state is 'finish':
+                self.change_state('finish')
+            elif self.subtask.state is 'error':
+                self.change_state('error')
+
+            # Don't forget to add this subtask to subtask book
--- new_nodes/task/basic_functional.py
+++ new_nodes/task/basic_functional.py
@@ -9,12 +9,11 @@ class BasicFunctional(AbstractTask):
 
     def perform(self, perception_data):
         if self.state is 'init':
-            # Do something
-            self.subtask = self.change_state_with_subtask('moveToPick&Place', 'MoveToLocation')
+            self.subtask = self.change_state_with_subtask('moveToPickPlace', 'MoveToLocation')
             if self.subtask is not None:
-                self.subtask.move.to_location('pick&place')
+                self.subtask.to_location('pickPlace')
 
-        elif self.state is 'moveToPick&Place':
+        elif self.state is 'moveToPickPlace':
             if self.subtask.state is 'finish':
                 self.subtask = None
                 self.subtask = self.change_state_with_subtask('grab', 'Grab')
@@ -40,7 +39,7 @@ class BasicFunctional(AbstractTask):
                 self.subtask = None
                 self.subtask = self.change_state_with_subtask('10question', 'MoveToLocation')
                 if self.subtask is not None:
-                    self.subtask.move.to_location('Final')
+                    self.subtask.to_location('Final')
 
         elif self.state is '10question':
             if self.subtask.state is 'finish':
@@ -49,15 +48,12 @@ class BasicFunctional(AbstractTask):
 
         elif self.state is 'detect':
             # answer random question
-            # if done:
-             if self.subtask.state is 'finish':
-                self.subtask = None
-                self.subtask = self.change_state_with_subtask('exit', 'MoveToLocation')
-                if self.subtask is not None:
-                    self.subtask.move.to_location('exit')
+            if self.subtask.state is 'finish':
+                self.subtask = self.change_state_with_subtask('exit', 'Answer several question')
 
         elif self.state is 'exit':
-            if self.subtask.state is exit:
-                # say I will now stop
-                self.change_state('finish')
+            if self.subtask.state is 'finish':
+                self.subtask = None
+                self.subtask = self.change_state_with_subtask('finish', 'LeaveArena')
+
         # Don't forget to add task to task_book
