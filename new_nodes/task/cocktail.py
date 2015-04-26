89ce2a1712a5ac0bea8ea31a6376ae18e1a36cfd
Nut Kaewnak <NutKaewnak@users.noreply.github.com>
2015-04-27 01:49:27 +0700

    Merge changes and refactor cocktail state. Still have grabObjectToPerson to do.

--- new_nodes/task/cocktail.py
+++ new_nodes/task/cocktail.py
@@ -1,4 +1,5 @@
 __author__ = 'nicole'
+import rospy
 from include.abstract_task import AbstractTask
 
 
@@ -8,7 +9,7 @@ class Cocktail(AbstractTask):
         self.subtask = None
         self.objective = 'getOrder'
         self.personNameList = []
-        self.objectList = []
+        self.orderList = []
 
     def perform(self, perception_data):
         if self.state is 'init':
@@ -16,74 +17,35 @@ class Cocktail(AbstractTask):
                 self.change_state_with_subtask('movePassDoor', 'MovePassDoor')
 
         elif self.state is 'movePassDoor' or self.state is 'findAnotherOrder':
-            self.subtask = self.change_state_with_subtask('gotoKitchenRoom', 'MoveToLocation')
-            if self.subtask is not None:
-                self.subtask.to_location('kitchen counter')
+            self.subtask = self.change_state_with_subtask('findPeopleAndGetOrder', 'FindPeopleAndGetOrder')
 
-        elif self.state is 'gotoKitchenRoom':
-            if self.subtask.stage is 'finish':
-                if self.objective is 'getOrder':
-                    self.subtask = self.subtaskBook.get_subtask(self, 'FindPeopleUsingGesture')  # must make it
-                elif self.objective is 'serveOrder':
-                    self.subtask = self.change_state_with_subtask('searchPeople', 'CallPeopleAndFindUsingGesture')
-                    if self.subtask is not None:
-                        self.subtask.callFor(self.personNameList[0])
-                else:
-                    self.change_state('searchPeople')
+        elif self.state is 'findPeopleAndGetOrder':
+            if self.current_subtask.stage is 'finish':
+                self.personNameList.append(self.subtask.getPeople())
+                self.orderList.append(self.subtask.getOrder())
 
-        elif self.state is 'searchPeople':
-            if self.subtask.stage is 'finish':
-                person = self.subtask.getFoundPerson()
-                if person is None:
+                if self.subtask.getPeople() is None or self.subtask.getOrder() is None:
                     self.change_state('error')
-                self.subtask = self.change_state_with_subtask('getToPerson', 'MoveToPerson')  # must make it
-                if self.subtask is not None:
-                    self.subtask.ToPerson(person)
-
-        elif self.state is 'getToPerson':
-            if self.subtask.stage is 'finish':
-                self.subtask = None
-                if self.objective is 'getOrder':
-                    self.subtask = self.change_state_with_subtask('askForCommand', 'AskForCommand')  # must make it
-                elif self.objective is 'serveOrder':
-                    self.subtask = self.change_state_with_subtask('serve', 'ServeOrder')  # must make it
-
-        elif self.state is 'askForCommand':
-            if self.subtask.state is 'finish':
-                if (self.subtask.getName() is not None
-                        and self.subtask.getObject() is not None):
-                    self.personNameList.append(self.subtask.getName())
-                    self.objectList.append(self.subtask.getObject())
-                else:
-                    self.change_state('error')
+                    rospy.loginfo('Bug in subtask find people and get order')
 
                 if len(self.personNameList) < 3:
-                    self.change_state('findAnotherOrder')
+                    self.change_state('prepareToServePerson')
                 else:
-                    self.subtask = None
-                    self.subtask = self.change_state_with_subtask('goGetObject', 'MoveToLocation')
-                    if self.subtask is not None:
-                        self.subtask.to_location('hallway table')
+                    self.subtask.change_state('init')
 
-        elif self.state is 'goGetObject':
-            if self.subtask.state is 'finish':
-                if len(self.objectList) > 0:
-                    self.objective = 'serveOrder'
-                    self.subtask = self.change_state_with_subtask('grabObject', 'Grab')  # must make it
-                    if self.subtask is not None:
-                        self.subtask.grab(self.objectList[0])
-                else:
-                    self.change_state_with_subtask('finish', 'LeaveArena')
+        elif self.state is 'prepareToServePerson':
+            self.subtask = self.change_state_with_subtask('getObjectAndServePerson', 'GrabObjectToPerson')
+            # must make it
 
+        elif self.state is 'getObjectAndServePerson':
+            if self.current_subtask.stage is 'finish':
+                if len(self.personNameList) == 0 or len(self.orderList) == 0:
+                    rospy.loginfo('done serve object')
+                    self.subtask = self.change_state('exit')
 
-        elif self.state is 'grabObject':
-            if self.subtask.state is 'finish':
-                self.subtask = self.change_state_with_subtask('gotoKitchenRoom', 'MoveToLocation')
-                if self.subtask is not None:
-                    self.subtask.to_location('kitchen counter')
+                person = self.personNameList.pop(0)
+                order = self.orderList.pop(0)
+                self.subtask.start(order, person)
 
-        elif self.state is 'serve':
-            if self.subtask.state is 'finish':
-                self.personNameList.pop(0)
-                self.objectList.pop(0)
-                self.change_state('goGetObject')
+        elif self.state is 'exit':
+            self.change_state_with_subtask('finish', 'LeaveArena')
