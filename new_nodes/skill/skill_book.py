461ac53afbc0e06c831b807738fb0bf78b68d03b
nicole <nicole@Nuts-MacBook-Pro.local>
2015-04-27 04:01:09 +0700

    Added detect people using gesture skill.

--- new_nodes/skill/skill_book.py
+++ new_nodes/skill/skill_book.py
@@ -4,6 +4,7 @@ from move_base_absolute import MoveBaseAbsolute
 from move_base_relative import MoveBaseRelative
 from rips_out import RipsOut
 from say import Say
+from detect_people_with_gesture import DetectPeopleWithGesture
 
 
 class SkillBook:
@@ -13,6 +14,7 @@ class SkillBook:
         self.book['MoveBaseRelative'] = MoveBaseRelative(control_module)
         self.book['RipsOut'] = RipsOut(control_module)
         self.book['Say'] = Say(control_module)
+        self.book['DetectPeopleWithGesture'] = DetectPeopleWithGesture(control_module)
 
     def get_skill(self, subtask, skill_name):
         self.book[skill_name].reset()
