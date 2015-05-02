6bba6fbec82c13b32f912a20c8aea0d9c97a694b
Krit Chaiso <krit.c@ku.th>
2015-05-02 10:43:01 +0700

    Add move_absolute subtask

--- new_nodes/subtask/move_absolute.py
+++ new_nodes/subtask/move_absolute.py
@@ -5,21 +5,21 @@ import rospy
 from include.abstract_subtask import AbstractSubtask
 
 
-class MoveRelative(AbstractSubtask):
+class MoveAbsolute(AbstractSubtask):
     def __init__(self, planning_module):
         AbstractSubtask.__init__(self, planning_module)
-        self.moveRelative = None
+        self.moveAbsolute = None
 
     def set_position(self, x, y, theta):
-        self.moveRelative = self.skillBook.get_skill(self, 'MoveBaseRelative')
-        self.moveRelative.set_position(x, y, theta)
+        self.moveAbsolute = self.skillBook.get_skill(self, 'MoveBaseAbsolute')
+        self.moveAbsolute.set_position(x, y, theta)
         self.change_state('move')
 
     def perform(self, perception_data):
         if self.state is 'move':
             # check if base succeed
-            if self.moveRelative.state is 'succeeded':
+            if self.moveAbsolute.state is 'succeeded':
                 self.change_state('finish')
-            elif self.moveRelative.state is 'aborted':
+            elif self.moveAbsolute.state is 'aborted':
                 rospy.loginfo('Aborted')
                 self.change_state('error')
