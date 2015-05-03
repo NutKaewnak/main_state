48ef90e9f20bda44a52aa1f3640ac0675f35f269
gerbera <gerbera@gerbera-tetra>
2015-05-04 03:12:39 +0700

    merge gerbera with desdemona.

--- new_nodes/subtask/introduce.py
+++ new_nodes/subtask/introduce.py
@@ -10,9 +10,5 @@ class Introduce(AbstractSubtask):
     def perform(self, perception_data):
         if self.state is 'init':
             self.skill = self.skillBook.get_skill(self, 'Say')
-            self.skill.say('Hello, my name is Lamyai. I am from Kasetsart university.')
-            self.change_state('rips_out')
-
-        elif self.state is 'rips_out':
-            self.skill = self.skillBook.get_skill(self, 'RipsOut')
+            self.skill.say('Hello, my name is Lamyai. I am cooking assistance.')
             self.change_state('finish')
