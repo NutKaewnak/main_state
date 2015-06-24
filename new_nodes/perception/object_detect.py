a4aa8edbeeb93f5d472ce2ef05b8264d00fbef4a
Nicole <Nicole@localhost>
2015-06-25 01:50:34 +0700

    test manipulate with move_it

--- new_nodes/perception/object_detect.py
+++ new_nodes/perception/object_detect.py
@@ -3,6 +3,7 @@ __author__ = 'Nicole'
 import rospy
 # from std_msgs.msg import String
 from geometry_msgs.msg import Point
+from object_detection.msg import ObjectDetection
 from include.abstract_perception import AbstractPerception
 from include.devices import Devices
 
@@ -10,7 +11,7 @@ from include.devices import Devices
 class ObjectDetect(AbstractPerception):
     def __init__(self, planning_module):
         AbstractPerception.__init__(self, planning_module)
-        rospy.Subscriber('/object_shapes', Point, self.callback_object_point)
+        rospy.Subscriber('/object_shape', ObjectDetection, self.callback_object_point)
 
     def callback_object_point(self, data):
         self.broadcast(Devices.OBJECT, data)
--- new_nodes/skill/pick.py
+++ new_nodes/skill/pick.py
@@ -1,5 +1,6 @@
 __author__ = 'Nicole'
 
+import rospy
 from include.abstract_skill import AbstractSkill
 from include.arm_status import ArmStatus
 from include.delay import Delay
@@ -20,7 +21,7 @@ class Pick(AbstractSkill):
         if self.state is 'init':
             self.change_state('waiting_for_goal')
 
-        elif self.state is 'prepare_to_pick':
+        elif self.state is 'prepare_to_pick': 
             self.manipulator.static_pose(self.side + '_arm', self.side + '_normal')  # may change
             self.change_state('arm_normal')
 
@@ -34,6 +35,7 @@ class Pick(AbstractSkill):
 
         elif self.state is 'prepare_move_hand_to_front_of_object':
             self.manipulator.pickobject_pregrasp(self.side + '_arm', self.goal_name, self.goal_pose)
+            self.delay.wait(20)
             self.change_state('move_to_in_front_of_object')
 
         elif self.state is 'move_to_in_front_of_object':
@@ -41,6 +43,9 @@ class Pick(AbstractSkill):
                 state = ArmStatus.get_state_from_status(perception_data.input)
                 if state is 'succeeded':
                     self.change_state('prepare_to_open_gripper')
+                elif not delay.is_waiting():
+                    rospy.logwarn('move_to_in_front_of_object out of time!')
+                    self.change_state('prepare_move_hand_to_front_of_object')
 
         elif self.state is 'prepare_to_open_gripper':
             self.manipulator.pickobject_opengripper()
@@ -49,8 +54,12 @@ class Pick(AbstractSkill):
         elif self.state is 'open_gripper':
             if self.device is not None and perception_data.device is self.device:
                 state = ArmStatus.get_state_from_status(perception_data.input)
+                self.delay.wait(20)
                 if state is 'succeeded':
                     self.change_state('prepare_move_to_object')
+                elif not delay.is_waiting():
+                    rospy.logwarn('open_gripper out of time!')
+                    self.change_state('prepare_to_open_gripper')
 
         elif self.state is 'prepare_move_to_object':
             # problem here
