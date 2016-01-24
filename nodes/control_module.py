9787d7e9ff25c2cee6e8d0275ceb90da98ece85c
Krit Chaiso <krit.c@ku.th>
2016-01-24 14:54:30 +0700

    update pick skill and subtask

--- nodes/control_module.py
+++ nodes/control_module.py
@@ -3,9 +3,9 @@ from controller.base_controller import BaseController
 from controller.neck_controller import NeckController
 from controller.torso_controller import TorsoController
 from controller.arm_controller import ArmController
-# from controller.manipulator_controller import ManipulateController
-# from controller.gripper_controller import GripperController
-from controller.recognize_objects_contoller import RecognizeObjectsObController
+from controller.manipulator_controller import ManipulateController
+from controller.gripper_controller import GripperController
+# from controller.recognize_objects_contoller import RecognizeObjectsObController
 
 __author__ = "AThousandYears"
 
@@ -17,9 +17,9 @@ class ControlModule:
     torso = TorsoController()
     left_arm = ArmController("left")
     right_arm = ArmController("right")
-    recognize_objects = RecognizeObjectsObController()
-    # manipulator = ManipulateController()
-    # gripper = GripperController()
+    # recognize_objects = RecognizeObjectsObController()
+    manipulator = ManipulateController()
+    gripper = GripperController()
 
     def __init__(self):
         pass
--- nodes/subtask/pick.py
+++ nodes/subtask/pick.py
@@ -9,14 +9,17 @@ class Pick(AbstractSubtask):
         AbstractSubtask.__init__(self, planning_module)
         self.skill = self.skillBook.get_skill(self, 'Pick')
         self.input_object_pos = None
-        self.side_arm = None
+        self.side_arm = 'right'
         self.move_base = None
 
     def perform(self, perception_data):
         if self.state is 'setting_arm':
-            self.skill.pick_object(self.side_arm, self.input_object_pos)
+            self.skill.pick_object(self.side_arm)
+            self.change_state('wait_skill_for_open_gripper')
+
+        elif self.state is 'wait_skill_for_open_gripper':
             if self.skill.state is 'open_gripper':
-                self.move_base = self.subtaskBook.get_subtask(self, 'MoveAbsolute')
+                self.move_base = self.subtaskBook.get_subtask(self, 'MoveRelative')
                 self.move_base.set_position(0.5, 0, 0)
             self.change_state('receive_object')
 
@@ -25,7 +28,8 @@ class Pick(AbstractSubtask):
             if self.skill.state is 'succeed':
                 self.change_state('finish')
 
-    def pick_object(self, side, goal):
-        self.input_object_pos = goal
+    def pick_object(self, side):
+        rospy.loginfo('------in pick_object:subtask--')
+        # self.input_object_pos = goal
         self.side_arm = side
         self.change_state('setting_arm')
