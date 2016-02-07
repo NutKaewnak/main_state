489ff1abdfd3fb8bcab5caa4393d82803c3c7eba
gerbera <gerbera@gerbera-tetra>
2016-02-07 10:54:22 +0700

    pick object by manipulate

--- nodes/control_module.py
+++ nodes/control_module.py
@@ -5,8 +5,8 @@ from controller.torso_controller import TorsoController
 from controller.arm_controller import ArmController
 from controller.manipulator_controller import ManipulateController
 from controller.gripper_controller import GripperController
-from controller.recognize_objects_contoller import RecognizeObjectsObController
-from controller.object_3d_detector_contoller import Object3dsDetectorContoller
+#from controller.recognize_objects_contoller import RecognizeObjectsObController
+#from controller.object_3d_detector_contoller import Object3dsDetectorContoller
 
 __author__ = "AThousandYears"
 
@@ -18,10 +18,10 @@ class ControlModule:
     torso = TorsoController()
     left_arm = ArmController("left")
     right_arm = ArmController("right")
-    recognize_objects = RecognizeObjectsObController()
+    #recognize_objects = RecognizeObjectsObController()
     manipulator = ManipulateController()
     gripper = GripperController()
-    object_3d_detector = Object3dsDetectorContoller()
+    #object_3d_detector = Object3dsDetectorContoller()
 
     def __init__(self):
         pass
