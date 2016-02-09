93e38cada030d992a47bea7dd7a1f7aa75ff4147
Nut Kaewnak <NutKaewnak@users.noreply.github.com>
2016-02-09 23:42:04 +0700

    commit from eureka before format

--- nodes/perception/people_detect.py
+++ nodes/perception/people_detect.py
@@ -1,7 +1,8 @@
 import rospy
+import tf
 from include.abstract_perception import AbstractPerception
 from include.devices import Devices
-from people_detection.msg import  PersonObjectArray
+from people_detection.msg import PersonObjectArray
 
 __author__ = 'AThousandYears'
 
@@ -9,7 +10,10 @@ __author__ = 'AThousandYears'
 class PeopleDetection(AbstractPerception):
     def __init__(self, planning_module):
         AbstractPerception.__init__(self, planning_module)
+        self.tf_listener = tf.TransformListener()
         rospy.Subscriber("/people_detection/people_array", PersonObjectArray, self.callback_people_array)
 
     def callback_people_array(self, data):
+        person_array = self.tf_listener.transformPoint('odom', data)
+        print person_array.point
         self.broadcast(Devices.PEOPLE, data.persons)
