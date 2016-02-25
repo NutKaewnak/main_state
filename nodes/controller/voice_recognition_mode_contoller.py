950d383ba461938b259390a08f62b4baa7d2aae6
Frank <thanachot7@gmail.com>
2016-02-25 13:37:57 +0700

    forgot add files

---
+++ nodes/controller/voice_recognition_mode_contoller.py
@@ -0,0 +1,13 @@
+import rospy
+import actionlib
+from speech_processing.msg import VoiceRegGoal, VoiceRegAction
+from std_msgs.msg import String
+
+__author__ = "Frank"
+
+class VoiceRecognitionModeContoller:
+    def __init__(self):
+        self.voice_recognition_mode = actionlib.SimpleActionClient('/recognizer/voice_reg', Object3DsAction)
+
+    def set_new_goal(self):
+        self.voice_recognition_mode.send_goal(VoiceRegGoal())
