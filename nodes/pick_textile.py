f95c7727fa359b7261f8c8a3b03b3f2a4bcd695c
fon <fon@localhost>
2016-03-12 00:30:15 +0700

    update inverse kinematics

--- nodes/pick_textile.py
+++ nodes/pick_textile.py
@@ -19,6 +19,39 @@ class PickTextile:
         self.invKine()
 
     def invKine(self):
+        self.obj_pos = Point()
+        self.obj_pos.x = 0.6
+        self.obj_pos.y = -0.25
+        self.obj_pos.z = 0.6
+
+        rospy.loginfo("-----ARM NORMAL-----")
+        manipulator_ctrl.static_pose('right_normal')
+        raw_input()
+
+        rospy.loginfo("-----ARM PREPARE-----")
+        manipulator_ctrl.static_pose('right_picking_prepare')
+        raw_input()
+
+        rospy.loginfo("-----INIT POSITION-----")
+        invK.init_position(self.obj_pos)
+        raw_input()
+
+        rospy.loginfo("-----OPEN GRIPPER-----")
+        self.pub_right_gripper.publish(0.8)
+        raw_input()
+
+        out_angle = inverse_kinematics.inverse_kinematic(manipulator_ctrl.transform_point(self.obj_pos), 0)
+        raw_input()
+        print 'ANGLE = ' + str(out_angle['right_shoulder_1_joint'])
+        manipulator_ctrl.move_joint('right_shoulder_1_joint', inverse_kinematics.in_bound('right_shoulder_1_joint', out_angle['right_shoulder_1_joint']))
+        manipulator_ctrl.move_joint('right_shoulder_2_joint', inverse_kinematics.in_bound('right_shoulder_2_joint', out_angle['right_shoulder_2_joint']))
+        manipulator_ctrl.move_joint('right_elbow_joint', inverse_kinematics.in_bound('right_elbow_joint', out_angle['right_elbow_joint']))
+        manipulator_ctrl.move_joint('right_wrist_1_joint', inverse_kinematics.in_bound('right_wrist_1_joint', out_angle['right_wrist_1_joint']))
+        manipulator_ctrl.move_joint('right_wrist_2_joint', inverse_kinematics.in_bound('right_wrist_2_joint', out_angle['right_wrist_2_joint']))
+        manipulator_ctrl.move_joint('right_wrist_3_joint', inverse_kinematics.in_bound('right_wrist_3_joint', out_angle['right_wrist_3_joint']))
+        manipulator_ctrl.move_relative([0, 0, 0], [0, 0, 0])
+        self.pub_right_gripper.publish(0.1)
+        raw_input()
 
 
 if __name__ == '__main__':
