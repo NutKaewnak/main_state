4d5e33b4865b88a4457788387a177f5ab6343687
Nutk <Nutk@localhost>
2016-03-12 20:35:14 +0700

    commit for cin

--- nodes/subtask/turn_neck.py
+++ nodes/subtask/turn_neck.py
@@ -1,24 +1,29 @@
 from include.abstract_subtask import AbstractSubtask
-
+import rospy
 __author__ = 'nicole'
 
 
 class TurnNeck(AbstractSubtask):
     def __init__(self, planning_module):
         AbstractSubtask.__init__(self, planning_module)
-        self.skill = self.current_skill
+        self.skill = self.skillBook.get_skill(self, 'TurnNeck')
+        # self.skill.turn(0, 0)
 
     def perform(self, perception_data):
         if self.state is 'init':
             self.change_state('wait_for_command')
 
         elif self.state is 'receive_command':
-            if self.skill.state is 'succeed':
+            if self.skill.state is 'succeeded':
+                print 'Succeeded'
                 self.change_state('finish')
             elif self.skill.state is 'aborted':
                 self.change_state('error')
 
     def turn(self, pitch, yaw):
-        self.skill = self.skillBook.get_skill(self, 'TurnNeck')
+        self.skill.turn_relative(pitch, yaw)
+        self.change_state('receive_command')
+
+    def setPositon(self, pitch, yaw):
         self.skill.turn(pitch, yaw)
         self.change_state('receive_command')
