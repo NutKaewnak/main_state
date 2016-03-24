a88819c18f933cfcb905b85cdb9c79c6caeaa919
Frank <thanachot7@gmail.com>
2016-03-25 03:36:53 +0900

    init GPSR

--- nodes/task/GPSR.py
+++ nodes/task/GPSR.py
@@ -2,6 +2,7 @@ __author__ = 'nicole'
 import rospy
 from include.abstract_task import AbstractTask
 from include.command_extractor import CommandExtractor
+import utility
 
 
 class GPSR(AbstractTask):
@@ -10,6 +11,7 @@ class GPSR(AbstractTask):
         self.subtask = None
         self.command_extractor = CommandExtractor()
         self.command = None
+        self.pick = None
         self.say = self.subtaskBook.get_subtask(self, 'Say')
 
     def perform(self, perception_data):
@@ -49,18 +51,107 @@ class GPSR(AbstractTask):
                 if 'robot yes' in perception_data.input:
                     self.say = self.subtaskBook.get_subtask(self, 'Say')
                     self.say.say('OK, I will do it.')
-                    self.change_state('action_1')
+                    self.change_state('action')
                 elif 'robot no' in perception_data.input:
                     self.say = self.subtaskBook.get_subtask(self, 'Say')
                     self.say.say('Sorry, Please say again.')
                     self.change_state('wait_for_command')
 
-        elif self.state == 'action_1':
+        # elif self.state == 'action_1':
+        #     if self.say.state == 'finish':
+        #         self.subtask = self.subtaskBook.get_subtask(self, 'MoveToLocation')
+        #         if self.command[0].object is not None:
+        #             self.subtask.to_location(self.command[0].data)
+        #             self.change_state('finish')
+        #         elif self.command[1].object is not None:
+        #             self.subtask.to_location(self.command[1].data)
+        #             self.change_state('finish')
+        elif self.state == 'action':
             if self.say.state == 'finish':
-                self.subtask = self.subtaskBook.get_subtask(self, 'MoveToLocation')
-                if self.command[0].object is not None:
-                    self.subtask.to_location(self.command[0].data)
+                if len(self.command) > 0:
+                    goal = self.command[0]
+                    if goal.action == 'go':
+                        self.subtask = self.subtaskBook.get_subtask(self, 'MoveToLocation')
+                        self.subtask.to_location(goal.data)
+                        self.command.pop(0)
+                        self.change_state('moving')
+                    elif goal.action == 'grasp':
+                        self.pick = self.subtaskBook.get_subtask(self, 'Pick')
+                        self.pick.side_arm = 'right_arm'
+                        self.change_state('wait_for_arm_init')
+                    elif goal.action == 'tell':
+                        if goal.object in ['your name', 'the name of your team']:
+                            self.say.say(utility.tell_my_name())
+                            self.change_state('action')
+                        elif goal.object in ['the time', 'what time is it']:
+                            self.say.say(utility.tell_the_time())
+                            self.change_state('action')
+                        elif goal.object == 'the date today':
+                            self.say.say(utility.tell_the_date_today())
+                            self.change_state('action')
+                        elif goal.object == 'the date tomorrow':
+                            self.say.say(utility.tell_day_tomorrow())
+                            self.change_state('action')
+                        elif goal.object == 'the day of the month':
+                            self.say.say(utility.tell_the_day_of_the_month())
+                            self.change_state('action')
+                        elif goal.object == 'the day of the week':
+                            self.say.say(utility.tell_the_day_of_the_week())
+                            self.change_state('action')
+                        elif goal.object == 'affiliation':
+                            self.say.say(utility.tell_affiliation())
+                            self.change_state('action')
+                    elif goal.action == 'follow':
+                        self.subtaskBook.get_subtask(self, 'TurnNeck').turn_absolute(0, 0)
+                        self.change_state('wait_for_command_follow')
+                else:
                     self.change_state('finish')
-                elif self.command[1].object is not None:
-                    self.subtask.to_location(self.command[1].data)
-                    self.change_state('finish')
+
+        elif self.state == 'moving':
+            if self.subtask.state == 'finish':
+                self.change_state('action')
+
+        elif self.state is 'wait_for_arm_init':
+            if self.pick.state is 'wait_for_point':
+                rospy.loginfo('---in test---')
+                self.subtask = self.subtaskBook.get_subtask(self, 'TurnNeck')
+                self.subtask.turn_absolute(-0.4, 0.0)
+                self.change_state('wait_for_turn_neck')
+
+        elif self.state is 'wait_for_turn_neck':
+            if self.subtask.state is 'finish':
+                self.change_state('find_object')
+
+        elif self.state is 'find_object':
+            self.subtask = self.subtaskBook.get_subtask(self, 'ObjectsDetection')
+            self.subtask.start()
+            self.change_state('wait_for_object')
+
+        elif self.state is 'wait_for_object':
+            if self.subtask.state is 'finish':
+                self.object_goal = self.subtask.objects[0].point
+                self.change_state('pick')
+
+        elif self.state is 'pick':
+            self.current_subtask = self.pick
+            self.pick.pick_object(self.object_goal)
+            self.change_state('wait_for_pick')
+
+        elif self.state is 'wait_for_pick':
+            if self.pick.state is 'finish':
+                self.change_state('action')
+
+        elif self.state is 'wait_for_command_follow':
+            if perception_data.device is self.Devices.VOICE and 'follow me' in perception_data.input:
+                self.subtaskBook.get_subtask(self, 'Say').say('I will follow you.')
+                self.follow = self.subtaskBook.get_subtask(self, 'FollowMe')
+                self.change_state('follow_init')
+
+        elif self.state is 'follow_init':
+            self.follow.start()
+            self.change_state('follow')
+
+        elif self.state is 'follow':
+            # recovery follow
+            if perception_data.device is self.Devices.VOICE and 'stop' in perception_data.input:
+                self.change_state('wait_for_command_follow')
---
+++ nodes/task/utility.py
@@ -0,0 +1,60 @@
+from time import gmtime, strftime, localtime
+
+
+def tell_team_name():
+    return "The name of my team is Skuba."
+
+
+def tell_my_name():
+    return "My name is Lumyai."
+
+
+def tell_the_time():
+    return strftime("%H %M.", localtime())
+
+
+def tell_you_are_from():
+    return "I am come from Thailand."
+
+
+def tell_the_weather_today():
+    return "The weather today is clear with periodic clouds. The highest temperature is 13 degree Celsius. The lowest temperature is 4 degree Celsius"
+
+
+def tell_the_date_today():
+    return strftime("%A of %B, %Y.", localtime())
+
+
+def tell_day_today():
+    return strftime("Today is %A.", localtime())
+
+def tell_day_tomorrow():
+    return strftime("Today is %A.", localtime(60*60*24))
+
+
+def tell_where_are_we():
+    return "We are at Aichi Institute of Technology."
+
+
+def tell_affiliation():
+    return "I come from Kasetsart University."
+
+
+def tell_the_day_of_the_month():
+    return strftime("%d of %B.", localtime())
+
+def tell_the_day_of_the_week():
+    return strftime("Today is %A.", localtime())
+
+if __name__ == "__main__":
+    print tell_my_name()
+    print tell_team_name()
+    print tell_the_time()
+    print tell_the_date_today()
+    print tell_day_today()
+    print tell_the_weather_today()
+    print tell_where_are_we()
+    print tell_affiliation()
+    print tell_day_tomorrow()
+    print tell_the_day_of_the_month()
+    print tell_the_day_of_the_week()
