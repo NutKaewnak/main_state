9e8ac56f2b9da7561fcce307fc51d7018c8645d2
nutkaewnak <nut-boss@hotmail.com>
2016-06-12 11:32:52 +0700

    edit navigate perception

--- nodes/perception/navigate.py
+++ nodes/perception/navigate.py
@@ -13,22 +13,25 @@ class Navigate(AbstractPerception):
         AbstractPerception.__init__(self, planning_module)
         # rospy.Subscriber('/base/status', String, self.callback_base_status)
         rospy.Subscriber("/navigate/move_base_node/TrajectoryPlannerROS/local_plan", Path, self.callback_local_plan)
-        rospy.Subscriber("/navigation/move_base_node/TrajectoryPlannerROS/global_plan", Path, self.callback_global_plan)
+        # rospy.Subscriber("/navigation/move_base_node/TrajectoryPlannerROS/global_plan", Path, self.callback_global_plan)
         self.position = (0, 0, 0)
+        self.global_path = []
+        # self.local_path = []
 
-    def callback_local_plan(self, data):
-        self.broadcast(Devices.NAVIGATE, data)
-        position = data.feedback.base_pos.poses.pose.position
-        orientation = data.feedback.base_position.poses.pose.orientation
-        quaternion = (0, 0, orientation.z, orientation.w)
-        rpy_angle = euler_from_quaternion(quaternion)
-        self.position = (position.x, position.y, rpy_angle[2])
-
+    # def callback_local_plan(self, data):
+    #     self.broadcast(Devices.NAVIGATE, data)
+    #     position = data.feedback.base_pos.poses.pose.position
+    #     orientation = data.feedback.base_position.poses.pose.orientation
+    #     quaternion = (0, 0, orientation.z, orientation.w)
+    #     rpy_angle = euler_from_quaternion(quaternion)
+    #     self.position = (position.x, position.y, rpy_angle[2])
+    #     self.local_path = data.feedback.base_pos.poses.pose.position
     def callback_global_plan(self, data):
         self.broadcast(Devices.NAVIGATE, data)
-        position = data.feedback.base_pos.poses.pose.position
-        orientation = data.feedback.base_position.poses.pose.orientation
+        position = data.poses.pose.position
+        orientation = data.poses.pose.orientation
         quaternion = (0, 0, orientation.z, orientation.w)
         rpy_angle = euler_from_quaternion(quaternion)
         self.position = (position.x, position.y, rpy_angle[2])
+        self.global_path = data.poses
 
