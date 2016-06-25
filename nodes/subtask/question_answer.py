c6830be3846eb25d8fd789a638f6fee0ad57917c
nutkaewnak <nut-boss@hotmail.com>
2016-06-25 22:16:20 +0700

    update state

--- nodes/subtask/question_answer.py
+++ nodes/subtask/question_answer.py
@@ -14,7 +14,7 @@ class QuestionAnswer(AbstractSubtask):
         self.skill = self.current_skill
         self.counter = 1
         self.limit = 5
-        self.has_direction = False
+        # self.has_direction = False
         self.question = None
         self.timer = Delay()
 
@@ -22,17 +22,21 @@ class QuestionAnswer(AbstractSubtask):
         if self.state is 'init':
             self.move_base = self.skillBook.get_skill(self, 'MoveBaseRelative')
             self.skill = self.skillBook.get_skill(self, 'TurnNeck')
+            self.say = self.skillBook.get_skill(self, 'Say')
             self.skill.turn(0, 0)
             self.counter = 1
             self.change_state('turn_neck')
 
         elif self.state is 'turn_neck':
-            if self.skill.state == 'succeeded':
-                self.change_state('prepare_to_answer')
+            self.change_state('prepare_to_answer')
+            # print self.skill.state
+            # if self.skill.state == 'succeeded':
+            #     self.change_state('prepare_to_answer')
 
         elif self.state is 'prepare_to_answer':
             # print 'prepare-----------------------------------------'
-            if perception_data.device is self.Devices.HARK_SOURCE_FRONT and self.has_direction is False:
+            if perception_data.device is self.Devices.HARK_SOURCE_FRONT and self.question == None:
+            # if perception_data.device is self.Devices.HARK_SOURCE_FRONT and self.has_direction is False:
                 if perception_data.input.src:
                     temp = 99
                     checker = 0
@@ -56,10 +60,11 @@ class QuestionAnswer(AbstractSubtask):
                     if checker == 1:
                         print 'get direction-------------------------------'
                         print self.choosen_degree
-                        self.has_direction = True
+                        # if self.question != None:
+                        #     self.has_direction = True
 
-
-            if perception_data.device is self.Devices.HARK_SOURCE_BACK and self.has_direction is False:
+            if perception_data.device is self.Devices.HARK_SOURCE_BACK and self.question == None:
+            # if perception_data.device is self.Devices.HARK_SOURCE_BACK and self.has_direction is False:
                 if perception_data.input.src:
                     temp = 99
                     checker = 0
@@ -89,35 +94,44 @@ class QuestionAnswer(AbstractSubtask):
                     if checker == 1:
                         print 'get direction-------------------------------'
                         print self.choosen_degree
-                        self.has_direction = True
+                        # if self.question != None:
+                        #     self.has_direction = True
 
             if perception_data.device is 'VOICE':
-                print perception_data.device, "KUY KUY", perception_data.input
+                print perception_data.device, "----------", perception_data.input
                 if perception_data.input is 'robot stop':
                     self.change_state('finish')
                 elif perception_data.input == 'what color is cobalt':
                     self.question = perception_data.input
+
                     print 'get input voice---------------------------------------'
 
-            if self.has_direction is True:
+            if self.question != None and self.choosen_degree != None:
+            # if self.has_direction is True and self.question != None:
             # if self.question is not None and self.has_direction is True:
                 print 'change to turn----------------------------------------'
                 self.change_state('turning')
 
         elif self.state is 'turning':
-            self.move_base.set_position(0, 0, radians(self.choosen_degree))
+            self.move_base.set_position_without_clear_costmap(0, 0, radians(self.choosen_degree))
             self.timer.wait(5)
             self.change_state('answering')
 
         elif self.state is 'answering':
             # print self.move_base.state
             if not self.timer.is_waiting():
-                print 'Say========================================'
-                self.skillBook.get_skill(self, 'Say').say('the answer of the question what color is cobalt is blue.')
+                print 'Say Set========================================'
+                self.say.say('the answer of the question what color is cobalt is blue.')
                 # self.skill.say('The answer of the question ' + str(perception_data.input) + ' is ' +
                 #                answers_the_questions.answers(perception_data.input))
-                self.has_direction = False
+                # self.has_direction = False
                 self.question = None
+                self.choosen_degree = None
+                self.change_state('speaking')
+
+        elif self.state is 'speaking':
+            print 'Speak========================================'
+            if self.say.state == 'succeeded':
                 self.change_state('prepare_to_answer')
 
         # elif self.state is 'prepare_to_answer':
