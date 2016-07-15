a2ce5f64138695ac3318bd37d5fa035950780bc1
Frank <thanachote.v@ku.th>
2016-07-15 13:49:32 +0200

    add pub 'current state and running'

--- nodes/planning_module.py
+++ nodes/planning_module.py
@@ -1,9 +1,9 @@
 __author__ = "AThousandYears"
-
+import rospy
+from std_msgs.msg import String, Int64
 from skill.skill_book import SkillBook
 from subtask.subtask_book import SubtaskBook
 from task.task_book import TaskBook
-
 
 class PlanningModule:
 
@@ -15,6 +15,11 @@ class PlanningModule:
         self.subtaskBook = SubtaskBook(self)
         self.subtaskBook.set_subtask_book(self)
         self.taskBook = TaskBook(self)
+
+        # rospy.init_node('~current')
+        self.pub_state = rospy.Publisher('~state', String, queue_size=1)
+        self.pub_running = rospy.Publisher('~running', Int64, queue_size=1)
+        self.running = 0
 
     def set_perception(self, perception_module):
         self.skillBook.set_perception(perception_module)
@@ -28,4 +33,8 @@ class PlanningModule:
             if self.perceptionModule.delay.is_waiting():
                 return
         # print perception_data
+        self.running = self.running + 1
+        self.pub_state.publish(String(self.taskBook.book[self.task].state))
+        self.pub_running.publish(Int64(self.running))
         self.taskBook.book[self.task].act(perception_data)
+        self.running = self.running - 1
