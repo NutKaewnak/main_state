fe61936cb2ce94fe2eb4f8535b014a3811410472
Frank <thanachote.v@ku.th>
2016-07-19 02:15:08 +0700

    add delay 5 s for callback, when status == succeeded

--- nodes/perception/base_status.py
+++ nodes/perception/base_status.py
@@ -3,6 +3,8 @@ from tf.transformations import euler_from_quaternion
 from move_base_msgs.msg import MoveBaseActionResult, MoveBaseActionFeedback
 from include.abstract_perception import AbstractPerception
 from include.devices import Devices
+from delay import Delay
+import threading
 
 __author__ = "AThousandYears"
 
@@ -12,10 +14,19 @@ class BaseStatusPerception(AbstractPerception):
         AbstractPerception.__init__(self, planning_module)
         rospy.Subscriber('/navigation/move_base/result', MoveBaseActionResult, self.callback_base_status)
         rospy.Subscriber('/navigation/move_base/feedback', MoveBaseActionFeedback, self.callback_base_position)
-
         self.position = None
+        self.flag = False
+
+    def callback_delay(self):
+        self.broadcast(Devices.BASE_STATUS, 3)
+        self.flag = False
 
     def callback_base_status(self, data):
+        if data.status.status == 3:
+            if not self.flag:
+                self.flag = True
+                threading.Timer(5, self.callback_delay).start()
+            return
         self.broadcast(Devices.BASE_STATUS, data.status.status)
 
     def callback_base_position(self, data):
