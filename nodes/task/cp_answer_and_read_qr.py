a04e0a1617d57b0dda371cebf13a7c58151881d8
=Benjarut <benjarut@github.com>
2016-11-24 10:53:26 +0700

    commit for merge

--- nodes/task/cp_answer_and_read_qr.py
+++ nodes/task/cp_answer_and_read_qr.py
@@ -1,7 +1,9 @@
 from include.abstract_task import AbstractTask
 from include.delay import Delay
 from include import answer_question_cp, qr_read_cp, arm_control
+from std_srvs.srv import *
 import rospkg
+import rospy
 import os
 __author__ = 'cindy'
 
@@ -13,6 +15,15 @@ class CPAnswerAndReadQR(AbstractTask):
 
     def perform(self, perception_data):
         if self.state is 'init' and perception_data.device is self.Devices.STATE_FLOW:
+            print 'before service'
+            rospy.wait_for_service('/recognizer_grammar/mic_control_open')
+            rospy.wait_for_service('/recognizer_grammar/mic_control_close')
+            print 'finish wait service'
+            self.mic_control_open = rospy.ServiceProxy("/recognizer_grammar/mic_control_open", Empty)
+            self.mic_control_close = rospy.ServiceProxy("/recognizer_grammar/mic_control_close", Empty)
+            print 'ServiceProxy'
+            self.mic_control_close()
+
             self.delay = Delay()
             rospack = rospkg.RosPack()
             self.path = rospack.get_path('main_state')
@@ -22,62 +33,59 @@ class CPAnswerAndReadQR(AbstractTask):
             self.change_state('ask_for_command')
 
         elif self.state is 'ask_for_command' and perception_data.device is self.Devices.STATE_FLOW:
-            self.subtaskBook.get_subtask(self, 'Say').say('I\'m ready for command.')
+            self.subtask = self.subtaskBook.get_subtask(self, 'PlaySound')
+            self.subtask.play(os.path.join(self.path, 'openhouse_sound', 'imreadyforcommand.wav'))
             self.change_state('wait_for_command')
+            # self.change_state('confirm_ans')
 
         elif self.state is 'wait_for_command':
+            self.mic_control_open()
+            self.change_state('waiting_voice')
+
+        elif self.state is 'waiting_voice':
             if perception_data.device is self.Devices.VOICE and perception_data.input:
                 if perception_data.input == 'robot answer question':
-                    self.subtaskBook.get_subtask(self, 'Say').say('Want me to answer questions. Robot Yes or Robot No ?')
-                    self.change_state('confirm_ans')
-                elif perception_data.input == 'robot read q r':
-                    self.subtaskBook.get_subtask(self, 'Say').say('Want me to read q r. Robot Yes or Robot No ?')
-                    self.change_state('confirm_qr')
-                elif perception_data.input == 'robot lift tray':
-                    self.subtaskBook.get_subtask(self, 'Say').say('Want me to lift tray. Robot Yes or Robot No ?')
-                    self.change_state('confirm_lift')
-
-        elif self.state is 'confirm_ans':
-            if perception_data.device is self.Devices.VOICE and perception_data.input:
-                if 'robot yes' in perception_data.input:
-                    self.subtaskBook.get_subtask(self, 'Say').say('I\'m listening.')
+                    self.mic_control_close()
+                    self.subtask = self.subtaskBook.get_subtask(self, 'PlaySound')
+                    self.subtask.play(os.path.join(self.path, 'openhouse_sound', 'imListening.wav'))
                     self.change_state('answer_question')
-                elif 'robot no' in perception_data.input:
-                    self.change_state('ask_for_command')
-
-        elif self.state is 'confirm_qr':
-            if perception_data.device is self.Devices.VOICE and perception_data.input:
-                if 'robot yes' in perception_data.input:
-                    self.subtaskBook.get_subtask(self, 'Say').say('I\'m ready for read.')
+                elif perception_data.input == 'robot read q r':
+                    self.mic_control_close()
+                    self.subtask = self.subtaskBook.get_subtask(self, 'PlaySound')
+                    self.subtask.play(os.path.join(self.path, 'openhouse_sound', 'ReadQRCode.wav'))
                     self.change_state('check_qr')
-                elif 'robot no' in perception_data.input:
-                    self.change_state('ask_for_command')
-
-        elif self.state is 'confirm_lift':
-            if perception_data.device is self.Devices.VOICE and perception_data.input:
-                if 'robot yes' in perception_data.input:
-                    self.subtaskBook.get_subtask(self, 'Say').say('I\'m lifting.')
+                elif perception_data.input == 'robot supermarket mode':
+                    self.mic_control_close()
+                    self.subtask = self.subtaskBook.get_subtask(self, 'PlaySound')
+                    self.subtask.play(os.path.join(self.path, 'openhouse_sound', 'LiftingInSupermarket.wav'))
+                    self.delay.wait(5)
                     self.change_state('lift_mode')
-                elif 'robot no' in perception_data.input:
-                    self.change_state('ask_for_command')
 
         elif self.state is 'answer_question':
+            self.mic_control_open()
+            self.change_state('listening_ques')
+
+        elif self.state is 'listening_ques':
             if perception_data.device is self.Devices.VOICE and perception_data.input:
                 self.subtask = self.subtaskBook.get_subtask(self, 'PlaySound')
-                self.subtask.play(os.path.join(self.path, 'cp_sound/40_QA',
+                r = answer_question_cp.answers(perception_data.input)
+                print ">>>>>>>>>>>>>>>", r
+                # self.subtaskBook.get_subtask(self, 'Say').say(r)
+                self.subtask.play(os.path.join(self.path, 'openhouse_sound',
                                                answer_question_cp.answers(perception_data.input)))
                 self.delay.wait(10)
                 self.change_state('direct_asking')
 
         elif self.state is 'direct_asking':
-            if self.subtask.state is 'finish':
+            if not self.delay.is_waiting():
+                self.mic_control_close()
                 self.change_state('ask_for_command')
 
         elif self.state is 'check_qr':
             if perception_data.device is 'VOICE' and perception_data.input:
                 print perception_data.input
                 self.subtask = self.subtaskBook.get_subtask(self, 'PlaySound')
-                self.subtask.play(os.path.join(self.path, 'cp_sound/Promotion',
+                self.subtask.play(os.path.join(self.path, 'openhouse_sound',
                                                qr_read_cp.answers(perception_data.input)))
                 self.delay.wait(10)
                 self.change_state('finish_read')
@@ -86,14 +94,22 @@ class CPAnswerAndReadQR(AbstractTask):
             if not self.delay.is_waiting():
                 self.change_state('ask_for_command')
 
-        elif self.state is 'lift_mode':
-            self.right_arm.close()
-            self.left_arm.close()
-            self.change_state('finish_lift')
+        elif self.state is 'lift_mode' and perception_data.device is self.Devices.STATE_FLOW:
+            if not self.delay.is_waiting():
+                print 'close right arm'
+                self.right_arm.move_right_joint([0.5, 0, 0.2, 0, 1.2, 0])
+                self.right_arm.close()
+                print 'close left arm'
+                # self.left_arm.close()
+                self.change_state('finish_lift')
 
-        elif self.state is 'finish_lift':
+        elif self.state is 'finish_lift' and perception_data.device is self.Devices.STATE_FLOW:
+            self.mic_control_open()
+            self.change_state('waiting_after_lift')
+
+        elif self.state is 'waiting_after_lift':
             if perception_data.device is self.Devices.VOICE and perception_data.input:
-                if 'robot listen command' in perception_data.input:
-                    self.right_arm.close()
-                    self.left_arm.close()
+                if 'robot supermarket mode' in perception_data.input:
+                    self.right_arm.move_right_joint([0, 0, 0, 0, 1.2, 0])
+                    # self.left_arm.close()
                     self.change_state('ask_for_command')
--- nodes/task/cp_walk_open.py
+++ nodes/task/cp_walk_open.py
@@ -16,28 +16,35 @@ class CPWalkOpen(AbstractTask):
             self.delay = Delay()
             rospack = rospkg.RosPack()
             self.path = rospack.get_path('main_state')
-            self.subtaskBook.get_subtask(self, 'TurnNeck').turn_absolute(0, 0)
-            self.subtask = self.subtaskBook.get_subtask(self, 'MoveRelative')
-            self.subtask.set_position(2, 0, 0)
-            self.change_state('check_move')
+            self.change_state('wait_joy')
+
+        elif self.state is 'wait_joy':
+            if perception_data.device is self.Devices.JOY and perception_data.input:
+                print '2222222222'
+                if 'B' in perception_data.input and 'LB' in perception_data.input:
+                    print '3333333333'
+                    self.subtaskBook.get_subtask(self, 'TurnNeck').turn_absolute(0, 0)
+                    self.subtask = self.subtaskBook.get_subtask(self, 'MoveRelative')
+                    self.subtask.set_position_without_clear_costmap(0, 0, pi/4)
+                    self.change_state('check_move')
 
         elif self.state is 'check_move':
             if self.subtask.state is 'finish':
                 self.subtask = self.subtaskBook.get_subtask(self, 'PlaySound')
-                self.subtask.play(os.path.join(self.path, 'sound', 'greeting_eng.wav'))
+                self.subtask.play(os.path.join(self.path, 'cp_sound', 'inviteToBoot.wav'))
                 self.delay.wait(10)
                 self.change_state('move_back')
 
         elif self.state is 'move_back' and perception_data.device is self.Devices.STATE_FLOW:
             if not self.delay.is_waiting():
                 self.subtask = self.subtaskBook.get_subtask(self, 'MoveRelative')
-                self.subtask.set_position(-2, 0, 0)
-                self.change_state('check_move_back')
-
-        elif self.state is 'check_move_back':
-            if self.subtask.state is 'finish':
-                self.subtask = self.subtaskBook.get_subtask(self, 'MoveRelative')
-                self.subtask.set_position(1, 0, 0)
-                self.change_state('finish')
+                self.subtask.set_position_without_clear_costmap(0, 0, -pi/4)
+                self.change_state('wait_joy')
+        #
+        # elif self.state is 'check_move_back':
+        #     if self.subtask.state is 'finish':
+        #         self.subtask = self.subtaskBook.get_subtask(self, 'MoveRelative')
+        #         self.subtask.set_position_without_clear_costmap(0, 0, -pi)
+        #         self.change_state('finish')
                 # Don't forget to add task to task_book
             # Don't forget to create launch file
