de4383b7af137e5fb96c563c094c6fb29b6c816d
Benjarut <cincin_sripetch@hotmail.com>
2017-03-04 21:26:34 +0700

    edit web_commu

--- nodes/controller/web_commu.py
+++ nodes/controller/web_commu.py
@@ -9,13 +9,13 @@ port = 1883
 
 
 class WebCommu:
-	def __init__(self):
-		self.client= mqtt.Client()
-		self.client.connect(broker, port)
+    def __init__(self):
+        self.client= mqtt.Client()
+        self.client.connect(broker, port)
 
-	def set_data(self, status, table, order):
-		data = {'name': 'lumyai', 'status': status, 'table': table, 'order': order}
-		self.on_publish(data)
+    def set_data(self, status, table, order):
+        data = {'name': 'lumyai', 'status': status, 'table': table, 'order': order}
+        self.on_publish(data)
 
-	def on_publish(self, data):
-		self.client.public("/ROBOT", json.dumps(data))
+    def on_publish(self, data):
+        self.client.public("/ROBOT", json.dumps(data))
